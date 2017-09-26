15b6245cc1150c376e4f13c8f3f64d6a9950ab3e
Jonathan M Davis <jmdavis@jmdavisprog.com>
2017-09-26 12:54:58 -0600

    Initial commit

---
+++ source/dxml/parser/internal.d
@@ -0,0 +1,322 @@
+// Written in the D programming language
+
+/++
+    Copyright: Copyright 2017
+    License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+    Author:   Jonathan M Davis
+  +/
+module dxml.parser.internal;
+
+import std.range.primitives;
+import std.traits;
+
+package:
+
+// We have this rather than using std.utf.byCodeUnit, because
+// std.utf.byCodeUnit does not provide access to the underlying string.
+auto byCodeUnit(R)(R range)
+    if(isForwardRange!R && isSomeChar!(ElementType!R))
+{
+    static if(isNarrowString!R)
+        return ByCodeUnit!R(range);
+    else
+        return range;
+}
+
+struct ByCodeUnit(S)
+    if(isNarrowString!S && !is(S == enum))
+{
+    alias C = ElementEncodingType!S;
+
+    @property C front() @safe const pure nothrow @nogc
+    {
+        return orig[0];
+    }
+
+    @property bool empty() @safe const pure nothrow @nogc
+    {
+        return orig.empty;
+    }
+
+    void popFront() @safe pure nothrow @nogc
+    {
+        orig = orig[1 .. $];
+    }
+
+    auto save() @safe pure nothrow @nogc
+    {
+        return this;
+    }
+
+    size_t length() @safe const pure nothrow @nogc
+    {
+        return orig.length;
+    }
+
+    C opIndex(size_t i) @safe pure nothrow @nogc
+    {
+        return orig[i];
+    }
+
+    auto opSlice(size_t i, size_t j) @safe pure nothrow @nogc
+    {
+        return ByCodeUnit(orig[i .. j]);
+    }
+
+    S orig;
+}
+
+@system pure nothrow @nogc unittest
+{
+    import std.algorithm : filter;
+    import std.meta : AliasSeq;
+
+    foreach(C; AliasSeq!(char, wchar, dchar))
+    {
+        static if(is(C == char))
+            enum str = "hello";
+        else static if(is(C == wchar))
+            enum str = "hello"w;
+        else
+            enum str = "hello"d;
+
+        foreach(S; AliasSeq!(C[], const(C)[], immutable(C)[]))
+        {
+            static if(is(C == dchar))
+                static assert(is(typeof(byCodeUnit(cast(S)str)) == S));
+            else
+                static assert(is(typeof(byCodeUnit(cast(S)str)) == ByCodeUnit!(typeof(S.init[]))));
+
+            static assert(is(typeof(byCodeUnit(filter!"true"(cast(S)str))) == typeof(filter!"true"(cast(S)str))));
+        }
+
+        foreach(D; AliasSeq!(C, const C, immutable C))
+            static assert(is(typeof(byCodeUnit(FwdCharRange!D(cast(D[])str))) == FwdCharRange!D));
+    }
+}
+
+@safe pure nothrow @nogc unittest
+{
+    import std.meta : AliasSeq;
+
+    foreach(str; AliasSeq!("hello"c, "ディラン"c, "hello"w, "ディラン"w, "hello"d, "ディラン"d))
+    {
+        foreach(range; AliasSeq!(str, raCharRange(str)))
+        {
+            auto bcu = byCodeUnit(range);
+            assert(bcu.length == str.length);
+
+            auto bcu2 = bcu.save;
+            foreach(c; str)
+            {
+                assert(bcu.front == c);
+                bcu.popFront();
+            }
+
+            foreach(i, c; str)
+                assert(bcu2[i] == c);
+        }
+    }
+
+    auto str = "hello world";
+    auto bcu = byCodeUnit(str);
+    assert(str is bcu.orig);
+    str = str[4 .. $];
+    assert(str !is bcu.orig);
+    bcu.popFrontN(4);
+    assert(str is bcu.orig);
+}
+
+//------------------------------------------------------------------------------
+// Unit test helpers
+//------------------------------------------------------------------------------
+
+version(unittest):
+
+struct FwdCharRange(C)
+{
+public:
+
+    @property bool empty() @safe const pure nothrow @nogc
+    {
+        return _str.empty;
+    }
+
+    @property C front() @safe const pure nothrow @nogc
+    {
+        return _str[0];
+    }
+
+    void popFront() @safe pure nothrow @nogc
+    {
+        _str = _str[1 .. $];
+    }
+
+    auto save() @safe pure nothrow @nogc
+    {
+        return this;
+    }
+
+    this(C[] str)
+    {
+        _str = str;
+    }
+
+private:
+
+    C[] _str;
+}
+
+auto fwdCharRange(R)(R range)
+    if(isSomeString!R && !is(R == enum))
+{
+    return FwdCharRange!(ElementEncodingType!R)(range);
+}
+
+static assert(isForwardRange!(FwdCharRange!char));
+static assert(!isBidirectionalRange!(FwdCharRange!char));
+
+
+struct RACharRange(C)
+{
+public:
+
+    @property bool empty() @safe const pure nothrow @nogc
+    {
+        return _str.empty;
+    }
+
+    @property C front() @safe const pure nothrow @nogc
+    {
+        return _str[0];
+    }
+
+    void popFront() @safe pure nothrow @nogc
+    {
+        _str = _str[1 .. $];
+    }
+
+    auto save() @safe pure nothrow @nogc
+    {
+        return this;
+    }
+
+    @property C back() @safe const pure nothrow @nogc
+    {
+        return _str[$ - 1];
+    }
+
+    void popBack() @safe pure nothrow @nogc
+    {
+        _str = _str[0 .. $ - 1];
+    }
+
+    @property size_t length() @safe const pure nothrow @nogc
+    {
+        return _str.length;
+    }
+
+    C opIndex(size_t i) @safe const pure nothrow @nogc
+    {
+        return _str[i];
+    }
+
+    this(C[] str)
+    {
+        _str = str;
+    }
+
+private:
+
+    C[] _str;
+}
+
+auto raCharRange(R)(R range)
+    if(isSomeString!R && !is(R == enum))
+{
+    return RACharRange!(ElementEncodingType!R)(range);
+}
+
+static assert(isForwardRange!(RACharRange!char));
+static assert(isRandomAccessRange!(RACharRange!char));
+static assert(!hasSlicing!(RACharRange!char));
+
+
+struct RASCharRange(C)
+{
+public:
+
+    @property bool empty() @safe const pure nothrow @nogc
+    {
+        return _str.empty;
+    }
+
+    @property C front() @safe const pure nothrow @nogc
+    {
+        return _str[0];
+    }
+
+    void popFront() @safe pure nothrow @nogc
+    {
+        _str = _str[1 .. $];
+    }
+
+    auto save() @safe pure nothrow @nogc
+    {
+        return this;
+    }
+
+    @property C back() @safe const pure nothrow @nogc
+    {
+        return _str[$ - 1];
+    }
+
+    void popBack() @safe pure nothrow @nogc
+    {
+        _str = _str[0 .. $ - 1];
+    }
+
+    @property size_t length() @safe const pure nothrow @nogc
+    {
+        return _str.length;
+    }
+
+    C opIndex(size_t i) @safe const pure nothrow @nogc
+    {
+        return _str[i];
+    }
+
+    auto opSlice(size_t i, size_t j) @safe pure nothrow @nogc
+    {
+        return RASCharRange(_str[i .. j]);
+    }
+
+    auto opSlice(size_t i, size_t j) @safe const pure nothrow @nogc
+    {
+        return RASCharRange!(const C)(_str[i .. j]);
+    }
+
+    auto opSlice(size_t i, size_t j) @safe immutable pure nothrow @nogc
+    {
+        return RASCharRange!(immutable C)(_str[i .. j]);
+    }
+
+    this(C[] str)
+    {
+        _str = str;
+    }
+
+private:
+
+    C[] _str;
+}
+
+version(unittest) auto rasCharRange(R)(R range)
+    if(isSomeString!R && !is(R == enum))
+{
+    return RASCharRange!(ElementEncodingType!R)(range);
+}
+
+static assert(isForwardRange!(RASCharRange!char));
+static assert(isRandomAccessRange!(RASCharRange!char));
+static assert(hasSlicing!(RASCharRange!char));
